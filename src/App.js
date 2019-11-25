3b51064bdadf73b1bb2ffef038d0f70d59fb8fb0
LopezDP <davidplopez@live.com>
2019-11-25 01:30:38 -0500

    feat: Adding to client routing to setup landing page and access to additional t&c and privacy pages needed

--- src/App.js
+++ src/App.js
@@ -1,48 +1,109 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import Amplify from "aws-amplify";
-import config from "./config";
-import * as serviceWorker from "./serviceWorker";
+import React, { Component, Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import { withRouter } from "react-router";
+import Routes from "./Routes";
+import "./App.css";
+//import Navbar from "react-bootstrap/Navbar";
+//import Nav from "react-bootstrap/Nav";
+//import NavDropdown from "react-bootstrap/NavDropdown";
+//import Form from "react-bootstrap/Form";
+//import FormControl from "react-bootstrap/FormControl";
+//import Button from "react-bootstrap/Button";
+//import Image from "react-bootstrap/Image";
+// import { LinkContainer } from 'react-router-bootstrap';
+//import { Auth } from "aws-amplify";
 
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
-  },
-  Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID
-  },
-  API: {
-    endpoints: [
-      {
-        // NOTE: The API "name" is critical and used by aws-amplify
-        //       when an amplify API.post() method where the first
-        //       argument is the name of this API field!!!
-        name: "future-api",
-        endpoint: config.apiGateway.URL,
-        region: config.apiGateway.REGION
-      }
-    ]
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true,
+      userAccessRole: ""
+    };
   }
-});
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+  // TODO: dev branch change just to get on cicd for now
+  // TODO: test branch change just to get on cicd for now
+  // TODO: demo branch change just to get on cicd for now
+  // Use componentDidMount() to load the user session with Amplify
+  async componentDidMount() {
+    try {
+      // returns a promise, rest of app will wait for this response
+      // from amplify auth with cognito
+      //await Auth.currentSession();
+      // if 200 OK from above with session info then execute next
+      //this.userHasAuthenticated(true);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      // if OK pull role from cognito custom attribute and store in state
+      //this.userHasPermission("admin");
+    } catch ( err ) {
 
+      /*
+      if (err !== "No current user") {
+        // eslint-disable-next-line
+        alert(err);
+      }
+      */
+    }
+    // loading user session is asyn process, we need to make sure
+    // that our app does not change state when it first loads
+    // we wait to render until isAuthenticating is false
+    this.setState({
+      //isAuthenticating: false
+    });
+  }
+
+  /*
+  userHasPermission = accessRole => {
+    this.setState({
+      userAccessRole: accessRole
+    });
+  }
+  
+  userHasAuthenticated = authenticated => {
+    this.setState({
+      isAuthenticated: authenticated
+    });
+  }
+
+  // eslint-disable-next-line
+  handleLogout = async event => {
+    // returns a promise that will clear user session for logout
+    await Auth.signOut();
+    this.userHasAuthenticated(false);
+    // use history.push from react-router4 to redirect
+    // user during login flow
+    this.props.history.push("/login");
+  }
+  */
+
+  render() {
+    const childProps = {
+    /*
+      // below is authentication provided by cognito
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated,
+      // Below is permissions available and passed as props
+      // authorization!!
+      userAccessRole: this.state.userAccessRole,
+      userHasPermission: this.userHasPermission
+    */
+    };
+
+    return (
+      //!this.state.isAuthenticating &&
+      <div className="App container">
+        
+        { /* These are the application's routes */ }
+        <Routes childProps={ childProps } />
+      </div>
+    );
+  }
+}
+
+// export with a HOC to give app access to router props
+// access the history object's props and the closest
+// <Routes>'s match withRouter().
+export default withRouter(App);
